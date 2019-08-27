7a5ec7a63b8c0650cdb1ccd56fa45504b72d68ac
Samuel del Rosario <samueldelrosario@MacBook-Air-de-Samuel.local>
2019-08-27 16:34:56 -0400

    primer commit de mi sitio web

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+	var altura = $('.header').offset().top;
+
+	$(window).on('scroll', function(){
+		if($(window).scrollTop() > altura){
+			$('.header').addClass('header-sombra');
+		}else{
+			$('.header').removeClass('header-sombra');
+		}
+	})
+
+
+
+// comienzo para la funcion del slider ===========================================
+
+	var imgItems = $('.cont-image li').length; //numero de slider
+	var imgPos = 1;
+
+	//agregando la paginacion
+	for(i = 1; i <= imgItems; i++){
+		$('.cont-slider-2').append('<div class="pagination"></div>');
+	}
+
+	$('.cont-image li').hide();
+	$('.cont-image li:first').show();
+
+
+	//FUNCIONES
+	$('.cont-slider-2 div').click(pagination);
+	$('.flecha-2').click(nextSlider);
+	$('.flecha-1').click(prevSlider);
+	
+	//EJECUTANDO FUNCIONES 
+	function pagination(){
+		var paginationPos = $(this).index() + 1;
+		console.log(paginationPos);
+	
+		$('.cont-image li').hide();
+		$('.cont-image li:nth-child('+ paginationPos +')').fadeIn();
+
+		//dandole estilo a la paginaccion seleccionada
+		$('.cont-slider-2 div').css({'background-color': '#2B2B2B'});
+		$(this).css({'background-color': "#8ECC6C"});
+	}
+
+	function nextSlider(){
+		if(imgPos >= imgItems){
+			imgPos = 1;
+		}else{
+			imgPos++;
+		}
+
+		$('.cont-slider-2 div').css({'background-color': '#2B2B2B'});
+		$('.cont-slider-2 div:nth-child('+ imgPos +')').css({'background-color': "#8ECC6C"});
+
+		$('.cont-image li').hide();
+		$('.cont-image li:nth-child('+ imgPos +')').fadeIn();
+	}
+
+	function prevSlider(){
+		if(imgPos <= 1){
+			imgPos = imgItems;
+		}else{
+			imgPos--;
+		}
+
+		$('.cont-slider-2 div').css({'background-color': '#2B2B2B'});
+		$('.cont-slider-2 div:nth-child('+ imgPos +')').css({'background-color': "#8ECC6C"});
+
+		$('.cont-image li').hide();
+		$('.cont-image li:nth-child('+ imgPos +')').fadeIn();
+	}
+
+	// $('.info-photoshop').hide();
+	// $('.photoshop').hover(function(){
+	// 	// if($('.info-photoshop').)
+	// 	$('.info-photoshop').fadeIn();
+	// })
+});
---
+++ respuesta.php
@@ -0,0 +1,250 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Comptible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/css.css">
+    
+</head>
+<body>
+    <header class="header">
+        <div class="max-width-144">
+            <section class="cont-logo">
+                <!-- <img src="imagenes/LOGO-B.png" alt=""> -->
+                <section class="text-company" style="display: none;">
+                    <p style="font-weight: bold;">samuel</p>
+                    <p>Guerra</p>
+                </section>
+            </section>
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#aspecto-profesional">Aspecto Profesional</a></li>
+                <li><a href="#experiencia">Experiencia</a></li>
+                <li><a href="#portafolio">Portafolio</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
+<!-------------------------- NOMRBE seiken no blacksmith, quah zhi shou----------------------->
+    <a href="" name="inicio"></a>
+    <section class="cont-name">
+        <section class="max-width-144 display-grid-name">
+            <section>
+                <h1 class="title-name"> Samuel </h1>
+                <p>del Rosario</p>
+                <h2 class="what-who">Disenador Grafico - Modelador  3D - Animacion 2D y 3D - Frond End and Web designer - Ilustrador</h2>
+            </section>
+            <!-- <img src="imagenes/arnold.png" alt="" class="arnold"> -->
+            <div class="logo-circle"><img src="imagenes/LOGO-N.png" alt="">
+            </div>
+        </section>
+    </section>
+<!-------------------------- ASPECTO PROFESIONAL ----------------------->
+    <a href="" name="aspecto-profesional"></a>
+    <section class="cont-profesional cont-profesional-2">
+        <!-- <img src="imagenes/capa.png" alt="" class="capa"> -->
+        <section class="max-width-144">
+            <h2 class="aspect-professional">MANEJO PROFESIONAL</h2>
+            <P class="aspect-profesional-subTitle">Conocimientos dentro del area</P>
+            <section>
+                <table style="width:100%">
+                    <tr>
+                        <td>Illustrator</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>Photoshop</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>Indesig</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>maya 3D</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>Blender</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>After effects</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>Adobe Premiere</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>Wix</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>Html</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>CSS</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+
+                    <tr>
+                        <td>JS</td>
+                        <td><div class="barra"></div></td>
+                    </tr>
+                </table>
+
+            </section>
+            <section class="cont-habilitys">
+                <h2>Habilidades</h2>
+                <p class="text-habilitys">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis </p>
+            </section>
+        </section>
+    </section>
+
+<!-------------------------- CONTENEDOR PORTAFOLIO ----------------------->    
+    <a href="" name="portafolio"></a>
+    <section class="cont-portafolio">
+
+        <section class="max-width-144">
+            <h2 class="portafolio-text">PORTAFOLIO</h2>
+            <section class="cont-boxes">
+                <div>
+                    <div class="boxes">
+                        <div class="designer"></div>
+                    </div>
+                    <P class="activo">DISENO GRAFICO</P>
+                    <p class="descripcion-boxes-portafolio" style="color: white;">hacemos de ti y de tu empresa la imagen adecuada a traves del diseno.</p>
+                </div>
+
+                <div>
+                    <div class="boxes ">
+                        <div class="modeling"></div>
+                    </div>
+                    <P class="activo">MODELADO 3D</P>
+                    <p class="descripcion-boxes-portafolio" style="color: white;">Hacemos la representacion de tu producto mediente el esculpido 3d.</p>
+                </div>
+                <div>
+                    <div class="boxes">
+                        <div class="frond-end"></div>
+                    </div>
+                    <P class="activo">FROND-END</P>
+                    <p class="descripcion-boxes-portafolio" style="color: white;">Persona ue permite al usuario la interaccion con la informacion de la pagina web</p>
+                </div>
+                <div>
+                    <div class="boxes">
+                        <div class="animation"></div>
+                    </div>
+                    <P class="activo">ANIMACIONES 2D - 3D</P>
+                    <p class="descripcion-boxes-portafolio" style="color: white;">Te creo la ilusion de movimiento a traves de diferentes medios.</p>
+                </div>
+
+            </section>
+        </section>
+
+    </section>
+
+<!-------------------------- EXPERIENCIA PROFESIONAL ----------------------->
+    <a href="" name="experiencia"></a>
+    <section name="arriba" class="cont-experience">
+        <section class="max-width-144">
+            <h2 class="experience-text">EXPERIENCIA</h2>
+            <!--experience 1-->
+            <section class="cont-boxes-experience">
+                <section class="border-left-experience flex-end position-relative">
+                        <h2 class="year-experience">2014 - 2015</h2>
+                        <p class="company-experience">YETO COLOR</p>
+                        <p class="position-in-company">Impresion digital</p>
+                        <p class="description-experience">Comence a trabajar en la empresa como impresor digital. En algunas ocasiones hice de soporte como diseñador grafico para algunos clientes, haciendo mis primeros diseños. </p>
+                    <div class="circles-experience"></div>
+                </section>
+            </section>
+
+            <!--experience 2-->
+            <section class="cont-boxes-experience">
+                <section class="border-right-experience second-box-experience position-relative">
+                    <h2 class="year-experience">2014 - 2015</h2>
+                    <p class="company-experience">YETO COLOR</p>
+                    <p class="position-in-company">Impresion digital</p>
+                    <p class="description-experience">Comence a trabajar en la empresa como impresor digital. En algunas ocasiones hice de soporte como diseñador grafico para algunos clientes, haciendo mis primeros diseños. </p>
+                    <div class="circles-experience-second"></div>
+                </section>
+            </section>
+
+            <!--experience 3-->
+            <section class="cont-boxes-experience">
+                <section class="flex-end position-relative">
+                    <h2 class="year-experience">2014 - 2015</h2>
+                    <p class="company-experience">YETO COLOR</p>
+                    <p class="position-in-company">Impresion digital</p>
+                    <p class="description-experience">Comence a trabajar en la empresa como impresor digital. En algunas ocasiones hice de soporte como diseñador grafico para algunos clientes, haciendo mis primeros diseños. </p>
+                    <div class="circles-experience"></div>
+                </section>
+            </section>
+        </section>
+    </section>
+
+<!-------------------------- CONTACTO ----------------------->
+    <a href="" name="contacto"></a>
+    <section class="cont-contact">
+        <section class="max-width-144">
+            <section class="prueba">
+                <section class="contact">
+                    <h2 class="contact-text">CONTACTO</h2>
+                    <section class="formulario-contact">
+                        <p class="description-contact">Escribeme para saber cualquier informacion que necesites</p>
+                        <!-- <form action="respuesta.php" method="POST">
+                            <div class="name-email">
+                                <input type="text" placeholder="Nombre">
+                                <input type="text" placeholder="Email">
+                            </div>
+                            <input class="input-telephone" type="text" placeholder="Telefono" name="" id="">
+                            <textarea name="" id="" cols="30" rows="10"></textarea>
+                            <button class="submit">Enviar</button>
+                        </form> -->
+
+                        <div>
+                            <h2 style="color: white">aprendiendo php</h2>
+                            <?php $resultado = $_POST; ?>
+                            <?php $nombre = $resultado["nombre"]; ?>
+                            <?php $apellido = $resultado["apellido"]?>
+                            <pre></pre>
+
+                            <?php 
+                            
+                            if (! (filter_has_var(INPUT_POST, 'nombre') && 
+                            (strlen (filter_input(INPUT_POST, 'nombre')) > 0))){
+                                echo "el nombre es obligatorio" ;
+                            } else {    ?>
+                                <!-- <p>nombre<?php echo $nombre ?></p> -->
+                                <p>El formulario ha sido llenado satisfactoriamente</p>
+                            
+                            <?php } ?>
+                        </div>
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+
+<!-------------------------- FOOTER ----------------------->
+    <footer class="cont-footer">
+        <p>por el momento no se que poner aqui</p>
+    </footer>
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="js/main.js"></script>
+</html>
